df6533c307c6355ad439be6bc09d54e17cd81650
Andrew Jones <andrewmjones@students.abtech.edu>
2026-09-07 13:39:09 -0400

    edit to inheritance

--- asgn02-inheritance/challenge-02-inheritance.php
+++ asgn02-inheritance/challenge-02-inheritance.php
@@ -1,17 +1,30 @@
 <?php
 class mini
 {
-  var  string $name = 'mini';
-  var int $creationDate;
+  protected  string $name = 'mini';
+  protected int $creationDate;
+  private $color = 'red';
   public function fullName()
   {
     return $this->name .  "<br>";
+  }
+
+  public function setDate($value) {
+    $this->creationDate = $value;
+  }
+
+  public function getDate() {
+    return $this->creationDate . "<br>";
+  }
+
+  public function colorChange($value) {
+    $this->color = $value;
   }
 }
 
 class cooper extends mini
 {
-  var string $tuning = 'cooper';
+  protected string $tuning = 'cooper';
   public function fullName()
   {
     return $this->name . $this->tuning .  "<br>";
@@ -20,7 +33,7 @@ class cooper extends mini
 
 class cooperS extends cooper
 {
-  var $trim = 's';
+  private $trim = 's';
   public function fullName()
   {
     return $this->name . $this->tuning . " " . $this->trim .  "<br>";
@@ -28,13 +41,18 @@ class cooperS extends cooper
 }
 
 $m = new mini;
-$m->creationDate = 1959;
-echo $m->fullName();
+$m->setDate(1959);
+echo 'This is a ' .$m->fullName();
+echo 'from' .$m->getDate();
 
 $c = new cooper;
-$c->creationDate = 1961;
-echo $c->fullName();
+$c->colorChange('british racing green');
+$c->setDate(1961);
+echo 'This is a ' . $c->fullName();
+echo 'from' . $c->getDate();
 
 $cs = new cooperS;
-$cs->creationDate = 2001;
-echo $cs->fullName();
+$cs->colorChange('yellow');
+$cs->setDate(2001);
+echo 'This is a' . $cs->fullName();
+echo 'from' . $cs->getDate();
