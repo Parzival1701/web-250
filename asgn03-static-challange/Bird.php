1d9bd16ecbdc2e28ad6ca381892cad38a2d08deb
Andrew Jones <andrewmjones@students.abtech.edu>
2026-09-14 09:08:34 -0400

    changed all parent class props

--- asgn03-static-challange/Bird.php
+++ asgn03-static-challange/Bird.php
@@ -1,31 +1,41 @@
 <?php
 
-class Bird {
-    var $habitat;
-    var $food;
-    var $nesting = "tree";
-    var $conservation;
-    var $song = "chirp";
-    var $flying = "yes";
+class Bird
+{
+  static public $instance_count;
 
-    function can_fly() {
-        if ( $this->flying == "yes" ) {
-            $flying_string = "can fly";
-        } else {
-            $flying_string = "is stuck on the ground";
-        }
-        return  $flying_string ;
+  static public $egg_num = 0;
+  var $habitat;
+  var $food;
+  var $nesting = "tree";
+  var $conservation;
+  var $song = "chirp";
+  var $flying = "yes";
+
+  public static function create() {}
+
+  public function can_fly()
+  {
+    if (static::$flying == "yes") {
+      $flying_string = "bird can fly";
+    } else {
+      $flying_string = "cannot fly and it stuck on the ground";
     }
+    return $flying_string;
+  }
 }
 
-class YellowBelliedFlyCatcher extends Bird {
-    var $name = "yellow-bellied flycatcher";
-    var $diet = "mostly insects.";
-    var $song = "flat chilk";
+class YellowBelliedFlyCatcher extends Bird
+{
+  
+  var $name = "yellow-bellied flycatcher";
+  var $diet = "mostly insects.";
+  var $song = "flat chilk";
 }
-
-class Kiwi extends Bird {
-    var $name = "kiwi";
-    var $diet = "omnivorous";
-    var $flying = "no";
+ 
+class Kiwi extends Bird
+{
+  var $name = "kiwi";
+  var $diet = "omnivorous";
+  var $flying = "no";
 }
